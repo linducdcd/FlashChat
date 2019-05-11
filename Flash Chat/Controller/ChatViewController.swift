5aa0e6ff5a244b40e1fc820e9c89ef710c08b898
Mehdi <linducdcd@gmail.com>
2019-05-11 03:41:49 -0700

    fixed chatViewController

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var messageTableView: UITableView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    
+    
     
     @IBAction func logOutPressed(_ sender: Any) {
         //TODO: Logout user and send to welcom screen
@@ -31,7 +39,15 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        
+        
+        
+        //TODO: Register MessageCell.xib
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTableView()
 
     }
 
@@ -40,7 +56,28 @@ class ChatViewController: UIViewController {
 
     }
     
+    //MARK: - tableView DataSource Methods
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        let messageArray = ["first, Hi" , "second" , "third"]
+        cell.messageBody.text = messageArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //TODO: Delclare numberOfRowInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
 
+    //TODO: Declare configureTableView
+    func configureTableView () {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+        
+    }
     
 
 }
---
+++ Flash Chat/View/Custom Cell/CustomMessageCell.swift
@@ -0,0 +1,20 @@
+//
+//  CusstomMessageCell.swift
+//  Flash Chat
+//
+//  Created by Lind Ucdcd on 5/10/19.
+//  Copyright © 2019 Lind Ucdcd. All rights reserved.
+//
+
+import UIKit
+
+class CustomMessageCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var avatorImagrView: UIImageView!
+    @IBOutlet weak var messageBackground: UIView!
+    @IBOutlet weak var messageBody: UILabel!
+    @IBOutlet weak var senderUserName: UIView!
+    
+}
